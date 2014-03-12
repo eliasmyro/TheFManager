da596594a847d8bd0b90bf274bdcdaf3094a7c72
Achilleas Naumidis <anaumidis@gmail.com>
2014-03-12 23:19:52 +0200

    Fixes icons. Fixes form dispose.

--- src/main/java/gr/teicm/mp/thefmanager/Main.java
+++ src/main/java/gr/teicm/mp/thefmanager/Main.java
@@ -2,15 +2,14 @@ package gr.teicm.mp.thefmanager;
 
 import gr.teicm.mp.thefmanager.gui.MainForm;
 
-/**
- *
- *
- */
+import javax.swing.*;
+
 public class Main
 {
     public static void main( String[] args )
     {
         MainForm mgrForm = new MainForm();
+        mgrForm.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         mgrForm.setVisible(true);
     }
 }
