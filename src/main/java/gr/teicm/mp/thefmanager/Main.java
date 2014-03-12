7601780387654cfb24fab299cba412e8b39c8d65
Achilleas Naumidis <anaumidis@gmail.com>
2014-03-13 01:12:17 +0200

    Fixes ugly font in Linux environment.

--- src/main/java/gr/teicm/mp/thefmanager/Main.java
+++ src/main/java/gr/teicm/mp/thefmanager/Main.java
@@ -3,13 +3,27 @@ package gr.teicm.mp.thefmanager;
 import gr.teicm.mp.thefmanager.gui.MainForm;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main
 {
     public static void main( String[] args )
     {
+        setUIFont(new javax.swing.plaf.FontUIResource(new Font("SansSerif", Font.PLAIN, 12)));
+
         MainForm mgrForm = new MainForm();
         mgrForm.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         mgrForm.setVisible(true);
+    }
+
+    private static void setUIFont(javax.swing.plaf.FontUIResource f) {
+        java.util.Enumeration keys = UIManager.getDefaults().keys();
+        while (keys.hasMoreElements()) {
+            Object key = keys.nextElement();
+            Object value = UIManager.get(key);
+            if (value instanceof javax.swing.plaf.FontUIResource) {
+                UIManager.put(key, f);
+            }
+        }
     }
 }
