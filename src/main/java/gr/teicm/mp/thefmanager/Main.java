77c749d9f0fee96a8a4ba697d24c7e3d5f290ae1
Achilleas Naoumidis <anaumidis@gmail.com>
2014-04-27 18:14:51 +0300

    Initial Commit.

--- src/main/java/gr/teicm/mp/thefmanager/Main.java
+++ src/main/java/gr/teicm/mp/thefmanager/Main.java
@@ -1,20 +1,17 @@
 package gr.teicm.mp.thefmanager;
 
-import gr.teicm.mp.thefmanager.controllers.preferences.*;
+import gr.teicm.mp.thefmanager.controllers.preferences.GetThemeName;
+import gr.teicm.mp.thefmanager.controllers.preferences.IGetThemeName;
+import gr.teicm.mp.thefmanager.controllers.preferences.IPutLastRunDate;
+import gr.teicm.mp.thefmanager.controllers.preferences.PutLastRunDate;
 import gr.teicm.mp.thefmanager.gui.MainForm.MainForm;
 
 import javax.swing.*;
-import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         IPutLastRunDate putLastRunDate = new PutLastRunDate();
         IGetThemeName getThemeName = new GetThemeName();
-
-        IGetLastRunDate getLastRunDate = new GetLastRunDate();
-        Date date = getLastRunDate.getTimestamp();
-
-        System.out.println(date.toString());
 
         putLastRunDate.putTimestamp();
 
