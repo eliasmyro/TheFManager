895c492f591646a7ced8b8c937b937ba3a83c325
Achilleas Naoumidis <anaumidis@gmail.com>
2014-05-05 04:27:17 +0300

    Initial Commit.

--- src/main/java/gr/teicm/mp/thefmanager/DAO/IFileSystemDAO.java
+++ src/main/java/gr/teicm/mp/thefmanager/DAO/IFileSystemDAO.java
@@ -6,7 +6,6 @@ import java.io.File;
  * Created by Achilleas Naoumidis on 3/24/14.
  */
 public interface IFileSystemDAO {
-        File[] getRoots();
-
-        File getHomeDirectory();
-        }
+    File[] getRoots();
+    File getHomeDirectory();
+}
